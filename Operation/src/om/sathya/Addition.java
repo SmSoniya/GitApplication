6355cb0d5b0a8e98bad34bfc15fc4af8ecd671c4
Your Name <you@example.com>
2023-05-31 02:41:47 -0700

    Additiobn

---
+++ Operation/src/om/sathya/Addition.java
@@ -0,0 +1,15 @@
+package om.sathya;
+
+public class Addition {
+	
+
+		public static void main(String[] args) {
+			 int n1=10;
+			 int n2=20;
+			 int n3=n1+n2;
+			 System.out.println("addition of two numbers"+n3);
+
+		}
+
+	}
+
